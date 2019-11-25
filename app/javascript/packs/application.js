1f61113ab1503a68f9d4b2df77910efb3b69f129
Stéphane <rabena.stephane@gmail.com>
2019-11-25 17:19:16 +0900

    Add application.scss and remove styles.scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,8 @@ require('@rails/activestorage').start();
 require('channels');
 
 import 'bootstrap/dist/js/bootstrap';
-import "@fortawesome/fontawesome-free/js/all";
+import '@fortawesome/fontawesome-free/js/all';
+import '../stylesheets/application.scss';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
