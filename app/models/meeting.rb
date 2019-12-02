a26d48c9f8d23b33ffaf6aa5edba40d84618bb6b
Stéphane <rabena.stephane@gmail.com>
2019-12-02 21:41:07 +0900

    Create initial meetings table and Meeting model

---
+++ app/models/meeting.rb
@@ -0,0 +1,3 @@
+class Meeting < ApplicationRecord
+
+end
---
+++ db/migrate/20191202112225_create_meetings.rb
@@ -0,0 +1,16 @@
+class CreateMeetings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :meetings do |t|
+      t.string :photo_filename
+      t.string :name
+      t.text :description
+      t.string :location
+      t.datetime :event_date
+      t.integer :duration
+      t.decimal :price
+      t.integer :max_attendees
+
+      t.timestamps
+    end
+  end
+end
