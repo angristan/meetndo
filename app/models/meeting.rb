5774d39f55fed2ec88a16cb1e060a0a5dca9c759
angristan <angristan@pm.me>
2019-12-08 19:38:16 +0900

    Fix meeting and user association

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,6 @@
 class Meeting < ApplicationRecord
   belongs_to :category, required: true
-  belongs_to :owner, class_name: 'User', required: true
+  belongs_to :owner, foreign_key: "user_id", class_name: 'User', required: true
 
   has_many :passive_attends, class_name: 'Attend', foreign_key: "attended_meeting_id", dependent: :destroy
   has_many :attendees, through: :passive_attends, source: :attendee
