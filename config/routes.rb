484689c2c83e64ef7d9db875c3fb97db3501d227
Stéphane <rabena.stephane@gmail.com>
2019-12-12 17:48:16 +0900

    Add my meetings page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get '/users/:id', to: 'users#index', as: 'profile'
 
+  get '/my-meetings', to: 'meetings#my_meetings', as: 'my_meetings'
+
   resources :meetings
   resources :attends, only: [:create, :destroy]
   resources :favorites, only: [:create, :destroy]
